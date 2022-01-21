9476ce3ce5ed0c13b50da1eb4785cd8be3d63d2c
Vhutshilo  Khomola <vhutshilo.khomola@dadevs.co.za>
2022-01-21 15:41:41 +0200

    all crud entry points working

--- index.js
+++ index.js
@@ -22,7 +22,17 @@ app.get("/todos",async (req,res) =>{
 
 //get a todo
 app.get("/todos/:id", async (req,res)=>{
+    const {id} =req.params;
+    try {
+        const todo = await pool.query(
+            "SELECT * FROM todo WHERE todo_id = $1",[id]
+            )
+            res.json(todo.rows[0])
         
+    } catch (err) {
+        console.error(err)
+    }
+
 });
 //create a todo
 
@@ -38,13 +48,41 @@ app.post("/todos", async(req,res)=>{
         
     } 
     catch (err) {
-        onsole.error(err.message)
+        console.error(err.message)
     }
         
-   
+//    0636290747
 })
 //update a todo
-//delete a todo
+app.put("/todos/:id", async (req,res) =>{
+    try {
+        const {id} = req.params;   //where
+        const {description} = req.body; //set
+
+        const updateTodo = await pool.query(
+            "UPDATE todo SET description = $1 WHERE todo_id = $2", [description,id]
+            )
+
+            res.json("Todo was updated!")
+    } catch (err) {
+        console.error(err.message)
+        
+    }
+})
+//delete a todo 
+
+app.delete("/todos/:id", async (req,res) => {
+    try {
+        const {id} = req.params;
+        const deleteTodo = await pool.query(
+            "DELETE FROM todo WHERE todo_id = $1 ",[id]
+            )
+            res.json("Todo was successfully deleted!")
+        
+    } catch (err) {
+        console.error(err.message)
+    }
+})
 
 
 const PORT = 4000;
